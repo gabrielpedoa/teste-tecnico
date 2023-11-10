4f8c11afaac8602df2f14ef051492b4cc9d3aa3b
Gabriel Antunes <gabrielpedoa@gmail.com>
2023-11-10 15:50:03 -0300

    ...

--- src/controllers/getClients.ts
+++ src/controllers/getClients.ts
@@ -2,8 +2,17 @@ import { Request, Response } from "express";
 import { prisma } from "../database";
 
 export default async (req: Request, res: Response) => {
+  const { take = 10, skip = 0, name } = req.query;
+  const getClients = await prisma.client.findMany({
+    where: {
+      name: {
+        contains: name as string,
+        mode: 'insensitive'
+      },
+    },
+    skip: Number(skip),
+    take: Number(take),
+  });
 
-  
-  const getClients = await prisma.client.findMany({});
   return res.status(200).json(getClients);
 };
